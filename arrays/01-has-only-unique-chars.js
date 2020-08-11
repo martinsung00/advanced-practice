abf93355952f7389c9e8d5951b84231460f227b8
Martin <martinsung00@gmail.com>
2020-08-10 21:44:22 -0700

    Complete has only unique characters toy problem

---
+++ arrays/01-has-only-unique-chars.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/*
+  Implement an algorithm to determine if a string has only unique characters, without using any additional data structures.
+  Input: "cat"
+  Output: true
+  Input: "greater good"
+  Output: false
+*/
+
+function hasOnlyUniqueCharacters(str) {
+// Create a storage to store unique characters. If the letter is in the storage already, and the letter appears again, return false
+  const storage = {};
+  const isUnique = true;
+
+  for (let i = 0; i < str.length; i++) {
+    let currentLetter = str[i].toString();
+
+    if(!storage[currentLetter]) {
+      storage[currentLetter] = 1;
+    } else {
+      return false
+    }
+  };
+  return isUnique;
+};
+
+// Runtime: O(n) || linear time
