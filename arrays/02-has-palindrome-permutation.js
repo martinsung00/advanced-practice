e2e55af9a3ba86e07fbe6e1548da27cd75ec6fed
Martin <martinsung00@gmail.com>
2020-08-14 21:59:16 -0700

    Remove count

--- arrays/02-has-palindrome-permutation.js
+++ arrays/02-has-palindrome-permutation.js
@@ -10,30 +10,21 @@
 
 export function hasPalindromePermutation(str) {
   /*
-  Edge cases: strings with spaces
   If string is of even length, every character should be paired, storage should be empty
   If string is of odd length there should be only one unique character
   */
   const storage = new Set();
-  let strCount = 0;
 
   for (let i = 0; i < str.length; i++) {
     let currentLetter = str[i];
 
     if (storage.has(currentLetter)) {
       storage.delete(currentLetter);
-      strCount++;
-    } else if (!storage.has(currentLetter) && currentLetter !== ' ') {
+    } else if (!storage.has(currentLetter)) {
       storage.add(currentLetter);
-      strCount++;
     }
   }
-
-  if (strCount & 1) {
-    return (storage.size === 1) ? true : false;
-  } else {
-    return (storage.size === 0) ? true : false;
-  }
+  return (storage.size <= 1) ? true : false;
 };
 
 // Runtime: O(n)
